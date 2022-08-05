a480488cab758f9111993a4facc8d65e873dda0b
Markus Wenzl <markus.wenzl@outlook.de>
2022-08-05 08:30:28 +0200

    Added hostname to network

--- AvaloniaInside/Network/Network.cs
+++ AvaloniaInside/Network/Network.cs
@@ -5,6 +5,23 @@ public static class Network
     public delegate void NetworkInterfaceOperationStateChangedHandler(
         NetworkInterfaceOperationStateChangedEvent eventargs);
 
+    private static string _hostName;
+
+    public static string HostName
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(_hostName))
+                _hostName = GetHostName();
+            return _hostName;
+        }
+        set
+        {
+            SetHostName(value);
+            _hostName = value;
+        }
+    }
+
     public static NetworkInterfaceOperationState DefaultInterfaceOperationState { get; private set; }
 
     /// <summary>
@@ -57,10 +74,33 @@ public static class Network
     /// <summary>
     ///     Starts the network daemon.
     /// </summary>
-    public static void StartDaemon() => SystemService.Start("S40network");
+    public static void StartDaemon()
+    {
+        SystemService.Start("S40network");
+    }
 
     /// <summary>
     ///     Stops the network daemon.
     /// </summary>
-    public static void StopDaemon() => SystemService.Stop("S40network");
+    public static void StopDaemon()
+    {
+        SystemService.Stop("S40network");
+    }
+
+    /// <summary>
+    ///     Gets the hostname
+    /// </summary>
+    /// <param name="hostName"></param>
+    private static void SetHostName(string hostName)
+    {
+        File.WriteAllText("/proc/sys/kernel/hostname", hostName);
+    }
+
+    /// <summary>
+    ///     Sets the hostname
+    /// </summary>
+    private static string GetHostName()
+    {
+        return File.ReadAllText("/proc/sys/kernel/hostname");
+    }
 }
